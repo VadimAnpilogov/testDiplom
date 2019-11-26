453f6dd0034fb951af3620e02cea4e67f4b91b70
VadimAnpilogov <vadick.anpilogov2015@yandex.ru>
2019-11-26 16:32:33 +0700

    проверка роли пользователя в меню

--- src/main/java/Project/config/MvcConfig.java
+++ src/main/java/Project/config/MvcConfig.java
@@ -1,13 +1,9 @@
 package Project.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {

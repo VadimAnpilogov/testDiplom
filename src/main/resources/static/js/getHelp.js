09cdbb4661305de4b3a72a38abfc59d470728081
Denis <denis.moroz.98@gmail.com>
2020-03-15 16:05:52 +0700

    Исправлен вывод на странице Помощь с поддержкой AJAX

--- src/main/resources/static/js/getHelp.js
+++ src/main/resources/static/js/getHelp.js
@@ -16,18 +16,37 @@
             url : window.location + "/all",
             success: function(result){
                 if(result.status == "Done"){
-                    $('#getResultDiv ul').empty();
-                    var custList = "";
+                    $('#getResultDiv').empty();
+
                     $.each(result.data, function(i, customer){
-                        var customer = "Id = " + i + ", Отзыв = " + customer.message +", Автор = " + customer.sender + ", дата = " + customer.date +"<br>";
-                        // var customer = "<div class=\"review\">\n" +
-                        //     "            <h4 class=\"reviewName\">customer.authorReviews</h4>\n" +
-                        //     "            <div class=\"reviewText\">\n" +
-                        //     "                <p>customer.reviewsOp</p>\n" +
-                        //     "            </div>\n" +
-                        //     "        </div>";
-                        $('#getResultDiv .list-group').append(customer)
+                        $('#getResultDiv').append("<div class='review'>" +
+                            "<h4 class='reviewName'>" + customer.sender + "</h4>" +
+                            "<span class='time_date'>" + customer.date + "</span>" +
+                            "<div class='reviewText'><p>" + customer.message + "</p></div></div>");
                     });
+
+
+
+                    // $("#getResultDiv").html("<div class='review'>\n" +
+                    //     "        <h4 class='reviewName'>" + customer.sender + "</h4>\n" +
+                    //     "        <span class='time_date'>" + customer.date + "</span>\n" +
+                    //     "        <div class='reviewText'>\n" +
+                    //     "            <p>" + customer.message + "</p>\n" +
+                    //     "        </div>\n" +
+                    //     "    </div>");
+
+
+                    // var custList = "";
+                    // $.each(result.data, function(i, customer){
+                    //     var customer = "Id = " + i + ", Отзыв = " + customer.message +", Автор = " + customer.sender + ", дата = " + customer.date +"<br>";
+                    //     // var customer = "<div class=\"review\">\n" +
+                    //     //     "            <h4 class=\"reviewName\">customer.authorReviews</h4>\n" +
+                    //     //     "            <div class=\"reviewText\">\n" +
+                    //     //     "                <p>customer.reviewsOp</p>\n" +
+                    //     //     "            </div>\n" +
+                    //     //     "        </div>";
+                    //     $('#getResultDiv .list-group').append(customer)
+                    // });
                     console.log("Success: ", result);
                 }else{
                     $("#getResultDiv").html("<strong>Error</strong>");

5b380e67af485c37c73e1530eb4e0b29e4df13a6
Denis <denis.moroz.98@gmail.com>
2020-03-17 22:24:05 +0700

    Исправление отображения сообщений и меню Создать диалог

--- src/main/resources/static/js/getMessage.js
+++ src/main/resources/static/js/getMessage.js
@@ -4,15 +4,29 @@ function ajaxGet(){
         url : window.location + "/all",
         success: function(result){
             if(result.status == "Done"){
+                var Enter = document.getElementById("enterID");
+                Enter.style.visibility = "visible";
                 $('#getResultMessage').empty();
+                var Username = document.getElementById("username").textContent;
 
                 $.each(result.data, function(i, customer){
-                    $('#getResultMessage').append(
-                        "<div class='received_msg'>" +
-                        "<div class='received_withd_msg'>" +
-                        "<p>" + customer.message + "</p>" +
-                        "<span class='time_dateInMes'>" + customer.date +
-                        "</span></div></div>");
+                    if (customer.sender !== Username) {
+                        $('#getResultMessage').append(
+                            "<div class='received_msg'>" +
+                            "<div class='received_withd_msg'>" +
+                            "<p>" + customer.message +"</p>" +
+                            "<span class='time_dateInMes'>" + customer.date +
+                            "</span></div></div>");
+                    }
+                    if (customer.sender === Username)
+                    {
+                        $('#getResultMessage').append(
+                            "<div class='outgoing_msg'>" +
+                            "<div class='sent_msg'>" +
+                            "<p>" + customer.message + "</p>" +
+                            "<span class='time_dateOutMes'>" + customer.date +
+                            "</span></div></div>");
+                    }
                 });
 
 // нужно сделать нормально
@@ -46,12 +60,15 @@ function ajaxGet(){
                 // });
                 console.log("Success: ", result);
             }else{
-                $("#getResultMessage").html("<strong>Error</strong>");
+                $("#getResultMessage").html("<strong>Error!</strong>");
                 console.log("Fail: ", result);
             }
         },
         error : function(e) {
-            $("#getResultMessage").html("<strong>Error</strong>");
+            $("#getResultMessage").html("<div class='getEnterMessage2'>" +
+                "<div class='getEnterMessage'>" +
+                "<img src='https://img.icons8.com/color/96/000000/chat.png'>" +
+                "<a>Выберите диалог</a></div></div>");
             console.log("ERROR: ", e);
         }
     });

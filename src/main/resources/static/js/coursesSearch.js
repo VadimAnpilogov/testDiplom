fe9e034dade8ec2f911b8d469833ac2d09d330e3
Denis <denis.moroz.98@gmail.com>
2020-03-20 20:28:51 +0700

    Добавлен поиск в разделе Курсы

--- src/main/resources/static/js/coursesSearch.js
+++ src/main/resources/static/js/coursesSearch.js
@@ -1,9 +1,9 @@
-function filterFunction() {
+function filterCourseName() {
     var input, filter, ul, li, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById("CourseNameID");
     filter = input.value.toUpperCase();
-    div = document.getElementById("mySidenav");
-    a = div.getElementsByTagName("div");
+    div = document.getElementById("coursesTilesID");
+    a = div.getElementsByTagName("tr");
     for (i = 0; i < a.length; i++) {
         if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
             a[i].style.display = "";
@@ -11,4 +11,13 @@ function filterFunction() {
             a[i].style.display = "none";
         }
     }
+}
+
+function openSearch() {
+    var Search = document.getElementById("searchCourses");
+    Search.style.display = "block";
+}
+function closeSearch() {
+    var Search = document.getElementById("searchCourses");
+    Search.style.display = "none";
 }
